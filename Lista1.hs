2f35362dea6de00fee7621fd315a6207b10efdfa
Lucas Anand Mazotti Ferretti <82159593+lucasanand9@users.noreply.github.com>
2023-04-04 16:59:31 -0300

    Add files via upload

--- Lista1.hs
+++ Lista1.hs
@@ -5,3 +5,16 @@ tipoTriangulo a b c = if a == b && a == c  then "equilatero" else if a == b || b
 triangulo a b c = if ehTriangulo a b c == False then "nao eh triangulo" else tipoTriangulo a b c
 
 
+somaPar 0 = 0
+somaPar a = if mod a 2 == 0 then somaPar' a else somaPar' (a-1) 
+somaPar' n = n + somaPar (n-2)
+
+pot x 0 = 1
+pot x n = x * pot x (n-1)
+somaPot2m 0 m = m
+somaPot2m n m = pot 2 n * m + somaPot2m (n-1) m
+
+primo' n 1 = True
+primo' n m = if mod n m == 0 then False else primo' n (m-1)  
+primo x =  primo' x (x-1)
+
